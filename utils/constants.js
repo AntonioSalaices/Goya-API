7fd619174bae7ce3c821d2e09343478c461c4046
Antonio Salaices Monzon <asalaicesm@concredito.com.mx>
2021-07-09 19:10:00 -0600

    new refresh token

--- utils/constants.js
+++ utils/constants.js
@@ -11,7 +11,7 @@ module.exports = {
   GCP: {
     client_id: '954539545044-2ubqrgmiuvc6pvpl3c3e8mjv4p66dhkq.apps.googleusercontent.com',
     secret_key: 'BOCbk6Pw8XPDa_tT_1lqo06D',
-    refresh_token: '1//04K6OL4sxLXFzCgYIARAAGAQSNwF-L9IrYgukV9ARbvGlLI0sTWXXfTfFNy16r8yCGD0yhPO_G3YyRO1mxzBFQK2gpKjfXN3WJaw',
+    refresh_token: '1//043u9d31xK6l5CgYIARAAGAQSNwF-L9IrpDuUkw7PcnEI91LE1d_55cNRC60rTsMwu8si8N5AeX_jjsI9ZM2vHM6bBvNEAWo2DeQ',
     redirect_url: 'https://developers.google.com/oauthplayground',
     user: 'goya.envio@gmail.com'
   }
